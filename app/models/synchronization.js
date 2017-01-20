b03314d943643887b0a327db35c3286264e4a478
Matt Gardner <wilburnforce@gmail.com>
2017-01-19 21:20:06 -0500

    Custom endpoint

--- app/models/synchronization.js
+++ app/models/synchronization.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import { memberAction, collectionAction } from 'ember-api-actions';
 
 export default DS.Model.extend({
   'service-item-id': DS.attr('string'),
@@ -19,5 +20,6 @@ export default DS.Model.extend({
   "created_at": DS.attr('date'),
   "updated_at": DS.attr('date'),
   "ran_at": DS.attr('date'),
-  "run_at": DS.attr('date')
+  "run_at": DS.attr('date'),
+  sync_now: memberAction({ path: 'sync_now', type: 'get' }),
 });
